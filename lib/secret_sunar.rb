b1419a30aca29d04e975da985cf5741396ccc189
Sroop Calbayrac <sroop@bandcamp.com>
2023-11-26 11:12:34 +0000

    pass in family member objects into secret_sunar

--- lib/secret_sunar.rb
+++ lib/secret_sunar.rb
@@ -2,17 +2,8 @@ class SecretSunar
 
 	attr_reader(:family)
 
-	def initialize
-		@family = [
-			# {name: "Mum"},
-			# {name: "Dad"},
-			# {name: "Sroop"},
-			# {name: "Jean-Jacques"},
-			# {name: "Rain"},
-			# {name: "Andreas"},
-			# {name: "Amar"},
-			# {name: "Sonali"}
-		]
+	def initialize(family)
+		@family = family
 	end
 
 end
--- spec/secret_sunar_spec.rb
+++ spec/secret_sunar_spec.rb
@@ -2,11 +2,15 @@ require 'secret_sunar'
 
 describe 'SecretSunar' do
 	
-	let(:secret_sunar) { SecretSunar.new }
+	let(:mum) { double :family_member, name: "Mum", phone: 111, exclusions: ["Dad"] }
+	let(:sroop) { double :family_member, name: "Sroop", phone: 222, exclusions: ["Jean-Jacques"] }
+	let(:secret_sunar) { SecretSunar.new([mum, sroop]) }
 
-	it 'has all family members' do
+	it 'has family members' do
 		family = secret_sunar.family
-		expect(family).to eq([])
+		expect(family).to eq([mum, sroop])
 	end
+
+	
 
 end
