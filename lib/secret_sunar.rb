6a8d0aa4f6b8db9c63255aedd9bb527c45f2caf4
Sroop Calbayrac <sroop@bandcamp.com>
2023-11-26 13:27:33 +0000

    draw names

--- lib/secret_sunar.rb
+++ lib/secret_sunar.rb
@@ -8,7 +8,14 @@ class SecretSunar
 	end
 
 	def messaging_client
-		TextMessage.new
+		# TextMessage.new
+	end
+
+	def draw_names
+		family.reduce(family) do |available_family_options, member|
+			giftee = member.pick_from(available_family_options)
+			available_family_options.reject{|m| m.name == giftee.name}
+		end
 	end
 
 end
